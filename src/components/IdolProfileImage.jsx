2b71d20ad9590e6c4476c0f30c04671e2148f487
박연희 <pyeonh8@gmail.com>
2025-06-05 15:17:20 +0900

    Design : 반응형 미디어 추가

--- src/components/IdolProfileImage.jsx
+++ src/components/IdolProfileImage.jsx
@@ -2,10 +2,7 @@ function IdolProfileImage({ size = '70px', img, alt = 'idol profile', isSelect =
   const imgAlt = typeof alt !== 'string' || alt.trim() === '' ? 'idol profile' : alt;
 
   return (
-    <div
-      className={`${isSelect ? 'idol__img--select' : ''} ${className} idol__img`}
-      style={{ width: size, height: size }}
-    >
+    <div className={`${isSelect ? 'idol__img--select' : ''} ${className} idol__img`} style={{ '--profile-size': size }}>
       <img src={img} alt={imgAlt} />
     </div>
   );
