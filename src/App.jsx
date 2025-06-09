0393bc5500d1fcb32bd9e2f522bbd8b0707a7a7b
박연희 <pyeonh8@gmail.com>
2025-06-09 13:45:34 +0900

    Chore : 이미지 추가 및 APP.jsx 로그인, 회원가입 Route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Common from './Common.jsx';
 import MyPage from './pages/MyPage.jsx';
+import Login from './pages/LoginPage.jsx';
+import LogUp from './pages/LogUpPage.jsx';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route index element={<List />} />
           <Route path="common" element={<Common />} />
           <Route path="my" element={<MyPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logUp" element={<LogUp />} />
         </Routes>
       </div>
       <Footer />
